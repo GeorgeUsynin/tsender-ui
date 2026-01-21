f64adf7a686c2ee31172751051a6464cef526be8
George Usynin <angiejo03@gmail.com>
2026-01-21 17:19:26 +0700

    minor refactor

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -1,11 +1,11 @@
-"use client";
+"use client"
 
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { anvil, sepolia, zksync } from "wagmi/chains";
+import { getDefaultConfig } from "@rainbow-me/rainbowkit"
+import { anvil, sepolia, zksync } from "wagmi/chains"
 
 export default getDefaultConfig({
   appName: "TSender",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
   chains: [anvil, sepolia, zksync],
   ssr: false,
-});
+})
