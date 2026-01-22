36b75409efec328ba8dc5449f1a772452cc6b387
George Usynin <angiejo03@gmail.com>
2026-01-22 14:06:00 +0700

    refactor config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  distDir: "out",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "",
+  assetPrefix: "./",
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -3,11 +3,11 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit"
 import { anvil, sepolia, zksync } from "wagmi/chains"
 
-const isDev = process.env.NODE_ENV == "development"
+// const isDev = process.env.NODE_ENV == "development"
 
 export default getDefaultConfig({
   appName: "TSender",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-  chains: isDev ? [anvil, sepolia, zksync] : [sepolia, zksync],
+  chains: [anvil, sepolia, zksync],
   ssr: false,
 })
