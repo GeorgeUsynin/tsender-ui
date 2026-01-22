04c13b9306be620376c990840b8a9a0fe03a91c7
George Usynin <angiejo03@gmail.com>
2026-01-22 14:19:53 +0700

    fix anvil

--- src/rainbowKitConfig.tsx
+++ src/rainbowKitConfig.tsx
@@ -3,11 +3,11 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit"
 import { anvil, sepolia, zksync } from "wagmi/chains"
 
-// const isDev = process.env.NODE_ENV == "development"
+const isDev = process.env.NODE_ENV == "development"
 
 export default getDefaultConfig({
   appName: "TSender",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-  chains: [anvil, sepolia, zksync],
+  chains: isDev ? [anvil, sepolia, zksync] : [sepolia, zksync],
   ssr: false,
 })
